57e095cc8c5a8bcb010035262a721067e5bae4f4
Richa Jos <richajos34@berkeley.edu>
2024-07-09 13:47:01 -0700

    inital commit, set up spring initalizer

---
+++ src/test/java/com/example/recommendation_engine/RecommendationEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recommendation_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommendationEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
